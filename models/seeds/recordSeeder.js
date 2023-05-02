c152be1c6ec90a22380ba49365710a78242b7101
s1030905 <s1030905@outlook.com>
2023-05-02 23:24:26 +0800

    create seeders with Promise All

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,27 +1,17 @@
 const Record = require("../record")
 const db = require("../../config/mongoose")
-const SEED_Record = {
-  id: "0",
-  name: '午餐',
-  date: '2019.04.23',
-  amount: '60',
-  userId: "0",
-  categoryId: "4"
-}
+const SEED_Record = [{ id: "0", name: '午餐', date: '2019.04.23', amount: '60', userId: "0", categoryId: "4" },
+{ id: "1", name: '晚餐', date: '2019.04.23', amount: '60', userId: "0", categoryId: "4" },
+{ id: "2", name: '捷運', date: '2019.04.23', amount: '120', userId: "0", categoryId: "2" },
+{ id: "3", name: '電影:驚奇隊長', date: '2019.04.23', amount: '220', userId: "1", categoryId: "3" },
+{ id: "4", name: '租金', date: '2015.04.01', amount: '25000', userId: "0", categoryId: "1" }
+]
 
 db.once("open", () => {
-  Record.create({
-    id: SEED_Record.id,
-    name: SEED_Record.name,
-    date: SEED_Record.date,
-    amount: SEED_Record.amount,
-    userId: SEED_Record.userId,
-    categoryId: SEED_Record.categoryId
-  })
+  Promise.all(SEED_Record.map(item => Record.create({ id: item.id, name: item.name, date: item.date, amount: item.amount, userId: item.userId, categoryId: item.categoryId })))
     .then(() => {
       console.log("done")
       process.exit()
     })
     .catch(console.error)
 })
-
--- models/seeds/userSeeder.js
+++ models/seeds/userSeeder.js
@@ -1,27 +1,21 @@
 const User = require("../user")
 const db = require("../../config/mongoose")
-const SEED_USER = {
-  name: '廣志',
-  email: 'root@example.com',
-  password: '12345678'
-}
+const bcrypt = require("bcryptjs")
+const SEED_USER = [{ name: '廣志', email: 'root@example.com', password: '12345678', id: 0 },
+{ name: '小新', email: 'root2@example.com', password: '123456789', id: 1 }]
 
 db.once("open", () => {
-  User.collection.countDocuments()
-    .then(n => {
-      console.log(n)
-      return n
-    })
-    .then((n) => User.create({
-      id: n,
-      name: SEED_USER.name,
-      email: SEED_USER.email,
-      password: SEED_USER.password,
-    })
+  for (let i = 0; i < 2; i++) {
+    bcrypt.genSalt(10)
+      .then((salt) => bcrypt.hash(SEED_USER[i].password, salt))
+      .then(hash => User.create({
+        name: SEED_USER[i].name, email: SEED_USER[i].email,
+        password: hash, id: SEED_USER[i].id
+      }))
       .then(() => {
         console.log("done")
         process.exit()
       })
-      .catch(console.error))
-    .catch(console.error)
+      .catch(console.error)
+  }
 })
