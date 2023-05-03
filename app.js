0589f97ee90d97a6177b2dca219467ea866ddc38
s1030905 <s1030905@outlook.com>
2023-05-03 11:41:15 +0800

    PORT

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000
 const handlebars = require("express-handlebars")
 const session = require("express-session")
 const methodOverride = require("method-override")
